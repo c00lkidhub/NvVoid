1bae90343420e05b31200a93b33df0d2960bcdcb
c00lkidhub <artemm228906@gmail.com>
2026-04-17 21:14:04 +0300

    Remove empty line in basic.lua

--- basic.lua
+++ basic.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/c00lkidhub/NvVoid/refs/heads/main/code.lua"))()
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/c00lkidhub/NvVoid/refs/heads/main/main.lua"))()
