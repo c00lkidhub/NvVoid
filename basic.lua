ee4b8cd4ada58772518e9fe168e4faf7726bfa30
c00lkidhub <artemm228906@gmail.com>
2026-04-17 21:37:04 +0300

    Add additional script loading in basic.lua

--- basic.lua
+++ basic.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/c00lkidhub/NvVoid/refs/heads/main/code.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/c00lkidhub/NvVoid/refs/heads/main/main.lua"))()
+
